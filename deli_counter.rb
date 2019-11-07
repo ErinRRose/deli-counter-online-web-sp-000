9a16418c776bce5b96fb7cf78e1e21a86a6c34b7
Erin  Rose <errose10@gmail.com>
2019-11-07 02:32:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -14,7 +14,7 @@ def line(katz_deli)
 end
 
 def take_a_number(katz_deli, name)
-  puts "Welcome, #{name}. You are number #{} in line."
+  puts "Welcome, #{name}. You are number #{index+1} in line."
 end
 
 def now_serving
