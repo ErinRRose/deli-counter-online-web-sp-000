c40f40774a644a1267e2f1422e636c7b5daf2027
Erin  Rose <errose10@gmail.com>
2019-11-07 02:27:18 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,7 +3,7 @@ katz_deli = []
 def line(katz_deli)
   if katz_deli.count = 0
     then  puts "The line is currently empty."
-  else 
+  else
     position = "The line is currently:"
     katz_deli.each_with_index do |name, index|
       position += " #{index}. #{name}"
